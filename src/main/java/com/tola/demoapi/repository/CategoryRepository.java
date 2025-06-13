21029fb8771259013406ac5b8d03abe479d7d4b7
soytolaa <soy.tola9999@gmail.com>
2025-06-13 17:11:09 +0700

    auth only

--- src/main/java/com/tola/demoapi/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tola.demoapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.tola.demoapi.model.entities.Category;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
